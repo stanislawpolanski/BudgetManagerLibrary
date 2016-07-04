e394b219e67d00e2321fff4363f4d2f33fe62be4
StanislawPolanski <stanislaw.polanski@hotmail.com>
2016-07-04 23:38:25 +0200

    BudgetPositionList

---
+++ BudgetManagerLibrary/Budget.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetManagerLibrary
+{
+	class Budget
+	{
+		public BudgetPositionsList BPL = new BudgetPositionsList();
+
+		private float GetBalance()
+		{
+			return BPL.GetBalance();
+		}
+	}
+}
---
+++ BudgetManagerLibrary/BudgetPositionsList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BudgetManagerLibrary
+{
+	class BudgetPositionsList : List<BudgetPosition>
+	{
+		internal float? Balance;
+		public float GetBalance()
+		{
+			if (this.Balance == null)
+				this.ComputeBalance();
+			return (float)this.Balance;
+		}
+
+		internal void ComputeBalance()
+		{
+			this.ForEach(delegate (BudgetPosition bp)
+			{
+				Balance += bp.Value;
+			}
+			);
+		}
+	}
+}
