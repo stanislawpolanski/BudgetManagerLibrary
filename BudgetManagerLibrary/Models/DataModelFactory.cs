e1fad0131d3c43d315c21a27beaf2331e7bb0a20
StanislawPolanski <stanislaw.polanski@hotmail.com>
2016-07-18 16:37:32 +0200

    Data Model factory ready

---
+++ BudgetManagerLibrary/Models/DataModelFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetManagerLibrary.Models
+{
+	public class DataModelFactory
+	{
+		public enum DataModelSource { XML };
+
+		private readonly IDictionary<DataModelSource, IDataModel> DataModelTypes;
+		public DataModelFactory()
+		{
+			DataModelTypes = new Dictionary<DataModelSource, IDataModel>
+			{
+				{DataModelSource.XML, new XMLDataModel()}
+			};
+		}
+
+		public IDataModel CreateDataModel(DataModelSource modelName)
+		{
+			IDataModel dataModel;
+			DataModelTypes.TryGetValue(modelName, out dataModel);
+			return dataModel;
+		}
+	}
+}
